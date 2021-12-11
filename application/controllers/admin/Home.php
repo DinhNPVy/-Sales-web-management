d2bafe24cd5b5780e42cbe810f89c001800b9c7f
DinhNPVy <79675139+DinhNPVy@users.noreply.github.com>
2021-12-11 16:53:26 +0700

    upload home

--- application/controllers/admin/Home.php
+++ application/controllers/admin/Home.php
@@ -3,80 +3,46 @@ class Home extends MY_Controller
 {
     function index()
     {
-        // Tai cac file thanh phan
-        $this->load->model('user_model');
-        $this->load->model('transaction_model');
+        $this->load->model('slide_model');
+        $slide_list = $this->slide_model->get_list();
+
+        $this->data['slide_list'] = $slide_list;
+
+
+        // lay danh sach bai viet moi
         $this->load->model('news_model');
-        $this->load->model('product_model');
-        $this->load->model('contact_model');
-
-        //thong ke doanh thu ngay hom nay
-        $today = get_date(now());
-        $time = get_time_between($today);
-        $where = array(
-            'created <=' => $time['end'],
-            'created >=' => $time['start'],
-            'status'     => 1
-        );
-        $amount_to_day = $this->transaction_model->get_sum('amount', $where);
-        $this->data['amount_to_day'] = $amount_to_day;
-
-        // tong thu theo thang nay
-        $thangnay = get_date(now());
-        $time     = get_time_between($thangnay, '1');
-        $where = array(
-            'created <=' => $time['end'],
-            'created >=' => $time['start'],
-            'status'     => 2
-        );
-        $tongtien_thang = $this->transaction_model->get_sum('amount', $where);
-        $this->data['tongtien_thang'] = $tongtien_thang;
-
-        // lay tong doanh thu
-        $where = array('status' => 2);
-        $amount_total = $this->transaction_model->get_sum('amount', $where);
-        $this->data[$amount_total] = $amount_total;
-
-        // thong ke tong so doanh thu
-        $this->data['total_transaction'] = $this->transaction_model->get_total();
-        $this->data['total_product']     = $this->product_model->get_total();
-        $this->data['total_news']        = $this->news_model->get_total();
-        $this->data['total_user']        = $this->user_model->get_total();
-        $this->data['total_contact']     = $this->contact_model->get_total();
-
-        //kiem ra co thuc hien lọc kh 
-        $input = array();
-        $id = $this->input->get('id');
-        $id = intval($id);
-        $input['where'] = array();
-        if ($id > 0) {
-            $input['where']['id'] = $id;
-        }
-
-        // lay ra danh sách sản phẩm
-        $list = $this->transaction_model->get_list($input);
-        $this->data['list'] = $list;
-
-        $this->lang->load('vi', 'vietnamese');
         $input = array();
         $input['limit'] = array(6, 0);
+        $news_list = $this->news_model->get_list($input);
+        $this->data['news_list'] = $news_list;
+
+        // lay danh sach san pham mơi
+        $this->load->model('product_model');
+        $input = array();
+        $input['limit'] = array(5, 0);
+        $product_newsest = $this->product_model->get_list($input);
+        $this->data['product_newsest'] = $product_newsest;
+
+        $this->load->model('product_model');
+        $input = array();
+        $input['limit'] = array(12, 0);
+        $product_all = $this->product_model->get_list($input);
+        $this->data['product_all'] = $product_all;
+
         $input['order'] = array('buyed', 'DESC');
         $product_buy = $this->product_model->get_list($input);
         $this->data['product_buy'] = $product_buy;
 
 
+        //lay noi dung cua bien message
+        $message = $this->session->flashdata('message');
+        $this->data['message'] = $message;
 
+        # Tải bộ thư viện ngôn ngữ tiếng việt.
+        $this->load->helper('language');
+        $this->lang->load('vi', 'vietnamese');
 
-
-        $this->data['temp'] = 'admin/home/index';
-        $this->load->view('admin/main', $this->data);
-    }
-    // thuc hien dang xuat
-    function logout()
-    {
-        if ($this->session->userdata('login')) {
-            $this->session->unset_userdata('login');
-        }
-        redirect(admin_url('login'));
+        $this->data['temp'] = 'site/home/index';
+        $this->load->view('site/layout', $this->data);
     }
 }
